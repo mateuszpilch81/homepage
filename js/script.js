0b3d0eb756766e1dc8e0e97f95d502381719e2bf
Mateusz Pilch <mp@mkpilch.pl>
2022-12-17 20:53:26 +0100

    js change

--- js/script.js
+++ js/script.js
@@ -3,17 +3,14 @@
         console.log("Hello to all developers");
     }
     welcome();
-
+}
+{
     const buttonR = document.querySelector(".button");
     const pictureMP = document.querySelector(".picture");
     const theName = document.querySelector(".theName");
 
     buttonR.addEventListener("click", () => {
         pictureMP.classList.toggle("display");
-        if (pictureMP.classList.contains("display")) {
-            theName.innerText = "Pokaż"
-        } else {
-            theName.innerText = "Usuń"
-        }
+        theName.innerText = pictureMP.classList.contains("display") ? "Pokaż" : "Usuń";
     });
 }

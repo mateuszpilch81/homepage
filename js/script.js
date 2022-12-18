1f68e001527b3af4b37611350a7485cf47dfd24c
Mateusz Pilch <mp@mkpilch.pl>
2022-12-18 19:24:28 +0100

    js function init added

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@
     const welcome = () => {
         console.log("Hello to all developers");
     }
-    welcome();
-}
-{
+
     const buttonR = document.querySelector(".button");
     const pictureMP = document.querySelector(".picture");
     const theName = document.querySelector(".theName");
 
-    buttonR.addEventListener("click", () => {
-        pictureMP.classList.toggle("display");
-        theName.innerText = pictureMP.classList.contains("display") ? "Pokaż" : "Usuń";
-    });
+    const init = () => {
+        buttonR.addEventListener("click", () => {
+            pictureMP.classList.toggle("display");
+            theName.innerText = pictureMP.classList.contains("display") ? "Pokaż" : "Usuń";
+        });
+    }
+    init();
+    welcome();
 }

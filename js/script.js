1c20e01463177e856304792abd7f4c11386d5db0
Mateusz Pilch <mp@mkpilch.pl>
2022-12-22 18:00:41 +0100

    change js code

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@
         console.log("Hello to all developers");
     }
 
-    const buttonR = document.querySelector(".button");
-    const pictureMP = document.querySelector(".picture");
-    const theName = document.querySelector(".theName");
-
     const init = () => {
+        const buttonR = document.querySelector(".button");
+        const pictureMP = document.querySelector(".picture");
+        const theName = document.querySelector(".theName");
         buttonR.addEventListener("click", () => {
             pictureMP.classList.toggle("display");
             theName.innerText = pictureMP.classList.contains("display") ? "Pokaż" : "Usuń";
